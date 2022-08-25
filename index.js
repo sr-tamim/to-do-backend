b13aed51cbf09a7a9eb2ee633b6fe042d40fa60c
SR Tamim <saifur.tamim@gmail.com>
2022-08-26 00:00:45 +0600

    little change

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(express.json())
 app.get('/', (req, res) => res.send(`to-do app server is running on port ${port}`))
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6ghhv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
