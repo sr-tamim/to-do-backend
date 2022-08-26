90a91a100a657e38a55ee48789a57bfe93849fd8
SR Tamim <saifur.tamim@gmail.com>
2022-08-26 07:20:14 +0600

    test netlify

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express')
 const app = express()
 const { MongoClient } = require('mongodb')
 const cors = require('cors')
+const path = require('path');
+const serverless = require('serverless-http');
 const port = process.env.PORT || 5000
 require('dotenv').config()
 
 // middlewares
 app.use(cors())
 app.use(express.json())
+const router = express.Router()
 
-app.get('/', (req, res) => res.send(`to-do app server is running on port ${port}`))
+router.get('/', (req, res) => res.send(`to-do app server is running on port ${port}`))
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6ghhv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -21,22 +24,22 @@ async function run() {
 
         const tasksCollection = client.db('toDoApp').collection('tasks')
 
-        app.get('/tasks', async (req, res) => {
+        router.get('/tasks', async (req, res) => {
             const cursor = tasksCollection.find({})
             const result = await cursor.toArray()
             res.json(result)
         })
-        app.post('/tasks', async (req, res) => {
+        router.post('/tasks', async (req, res) => {
             const result = await tasksCollection.insertOne(req.body)
             res.json(result)
         })
-        app.delete('/tasks', async (req, res) => {
+        router.delete('/tasks', async (req, res) => {
             const { timeStamp } = req.body
             const query = { timeStamp };
             const result = await tasksCollection.deleteOne(query);
             res.json(result)
         })
-        app.put('/tasks', async (req, res) => {
+        router.put('/tasks', async (req, res) => {
             const changedTask = req.body
             delete changedTask._id
             const filter = { timeStamp: changedTask.timeStamp };
@@ -52,4 +55,10 @@ async function run() {
 
 run().catch(console.dir)
 
-app.listen(port, () => console.log(`listening to port http://localhost:${port}`))
+// app.listen(port, () => console.log(`listening to port http://localhost:${port}`))
+
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
+
+module.exports = app;
+module.exports.handler = serverless(app);
