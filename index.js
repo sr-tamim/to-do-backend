9a1a6e9171b7c0dbe416a4a68e19376d02bb3300
SR Tamim <saifur.tamim@gmail.com>
2022-08-26 08:04:24 +0600

    ready for netlify deploy

--- index.js
+++ index.js
@@ -18,44 +18,36 @@ router.get('/', (req, res) => res.send(`to-do app server is running on port ${po
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6ghhv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function run() {
-    try {
-        await client.connect()
-
-        const tasksCollection = client.db('toDoApp').collection('tasks')
-
-        router.get('/tasks', async (req, res) => {
-            const cursor = tasksCollection.find({})
-            const result = await cursor.toArray()
-            res.json(result)
-        })
-        router.post('/tasks', async (req, res) => {
-            const result = await tasksCollection.insertOne(req.body)
-            res.json(result)
-        })
-        router.delete('/tasks', async (req, res) => {
-            const { timeStamp } = req.body
-            const query = { timeStamp };
-            const result = await tasksCollection.deleteOne(query);
-            res.json(result)
-        })
-        router.put('/tasks', async (req, res) => {
-            const changedTask = req.body
-            delete changedTask._id
-            const filter = { timeStamp: changedTask.timeStamp };
-            const updateDoc = { $set: changedTask };
-            const result = await tasksCollection.updateOne(filter, updateDoc, { upsert: false });
-            res.json(result)
-        })
-    } finally {
-        // await client.close()
-    }
-}
 
 
-run().catch(console.dir)
+client.connect()
 
-// app.listen(port, () => console.log(`listening to port http://localhost:${port}`))
+const tasksCollection = client.db('toDoApp').collection('tasks')
+
+router.get('/tasks', async (req, res) => {
+    const cursor = tasksCollection.find({})
+    const result = await cursor.toArray()
+    res.json(result)
+})
+router.post('/tasks', async (req, res) => {
+    const result = await tasksCollection.insertOne(req.body)
+    res.json(result)
+})
+router.delete('/tasks', async (req, res) => {
+    const { timeStamp } = req.body
+    const query = { timeStamp };
+    const result = await tasksCollection.deleteOne(query);
+    res.json(result)
+})
+router.put('/tasks', async (req, res) => {
+    const changedTask = req.body
+    delete changedTask._id
+    const filter = { timeStamp: changedTask.timeStamp };
+    const updateDoc = { $set: changedTask };
+    const result = await tasksCollection.updateOne(filter, updateDoc, { upsert: false });
+    res.json(result)
+})
+
 
 app.use('/.netlify/functions/index', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
