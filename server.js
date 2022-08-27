5f283f1f53f7e6e233fff34dca648595a8dd3f9f
SR Tamim <saifur.tamim@gmail.com>
2022-08-27 07:23:42 +0600

    code comments & revision

--- server.js
+++ server.js
@@ -12,54 +12,81 @@ app.use(cors())
 app.use(express.json())
 const router = express.Router()
 
+// test api
 router.get('/', (req, res) => res.send(`to-do app server is running on port ${port}`))
 
-
+// mongodb uri of the cluster
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@tasks-cluster.0fwv4yi.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
-
+// connect mongodb
 client.connect()
 
+// get all tasks from given collection
 async function getAllTasks(collection) {
     const cursor = collection.find({})
     const result = await cursor.toArray()
     return result
 }
-
+// get all tasks api
 router.get('/tasks/:email', async (req, res) => {
+    // create/get the collection of tasks of given email
     const tasksCollection = client.db('to-do-srt').collection(req.params.email)
+
     const result = await getAllTasks(tasksCollection)
     res.json(result)
 })
+// add new single task api
 router.post('/tasks/:email', async (req, res) => {
+    // create/get the collection of tasks of given email
     const tasksCollection = client.db('to-do-srt').collection(req.params.email)
+
+    // add the task
     const result = await tasksCollection.insertOne(req.body)
+
     const allTasks = await getAllTasks(tasksCollection)
     res.json({ ...result, allTasks })
 })
+// add an array of new tasks api
 router.post('/tasks/addMultipleTasks/:email', async (req, res) => {
+    // create/get the collection of tasks of given email
     const tasksCollection = client.db('to-do-srt').collection(req.params.email)
+
+    // add multiple tasks
     const result = await tasksCollection.insertMany(req.body)
+
     const allTasks = await getAllTasks(tasksCollection)
     res.json({ ...result, allTasks })
 })
+// delete single task api
 router.delete('/tasks/:email', async (req, res) => {
+    // create/get the collection of tasks of given email
     const tasksCollection = client.db('to-do-srt').collection(req.params.email)
+
+    // find out the specific task to delete
     const { taskAddedTime } = req.body
     const query = { taskAddedTime };
+
+    // delete the task
     const result = await tasksCollection.deleteOne(query);
+
     const allTasks = await getAllTasks(tasksCollection)
     res.json({ ...result, allTasks })
 })
+// update single task api
 router.put('/tasks/:email', async (req, res) => {
+    // create/get the collection of tasks of given email
     const tasksCollection = client.db('to-do-srt').collection(req.params.email)
+
     const changedTask = req.body
     delete changedTask._id
+
     const filter = { taskAddedTime: changedTask.taskAddedTime };
     const updateDoc = { $set: changedTask };
+
+    // update the task
     const result = await tasksCollection.updateOne(filter, updateDoc, { upsert: false });
+
     const allTasks = await getAllTasks(tasksCollection)
     res.json({ ...result, allTasks })
 })
