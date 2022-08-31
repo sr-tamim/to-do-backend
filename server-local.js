b159098801f359aaf001f40274048851aed122a5
SR Tamim <saifur.tamim@gmail.com>
2022-08-31 15:34:55 +0600

    little change

--- server-local.js
+++ server-local.js
@@ -2,4 +2,4 @@
 
 const app = require('./server');
 
-app.listen(5000, () => console.log('Local app listening on port 5000!'));
+app.listen(process.env.PORT || 5000, () => console.log(`Local app listening on port ${process.env.PORT || 5000}!`));
